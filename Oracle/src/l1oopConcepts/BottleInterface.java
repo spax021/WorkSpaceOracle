bf6aa88245f61b5c50a6feab2c60a42c05c2e648
spax021 <malbaski.ns@gmail.com>
2016-03-14 22:14:14 +0100

    lekcija 2 i 3 zavrsena

---
+++ Oracle/src/l1oopConcepts/BottleInterface.java
@@ -0,0 +1,11 @@
+package l1oopConcepts;
+
+public interface BottleInterface {
+
+	void napuni(int kolicina);
+	
+	void isprazni(int kolicina);
+	
+	void ispis();
+	
+}
---
+++ Oracle/src/l3operators/Main.java
@@ -0,0 +1,56 @@
+package l3operators;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		
+		
+		
+		
+		
+	
+	/*
+	 * In the following program, explain why the value "6" is printed twice in a row:
+		
+		class PrePostDemo {
+    	public static void main(String[] args){
+        
+        int i = 3;
+        i++;
+        System.out.println(i);    // "4"
+        ++i;                     
+        System.out.println(i);    // "5"
+        System.out.println(++i);  // "6"
+        System.out.println(i++);  // "6"
+        System.out.println(i);    // "7"
+        
+        BECAUSE FIRST ITS USED ++i, IT MEANS DO THE INCREMENTATION RIGHT NOW
+        WITH i++, FIRST IT SHOW THE ORIGINAL VALUE, AFTER THAT IT IS INCREMENTED
+        
+    }
+}
+	 * */	
+		
+		
+		
+	}
+
+//	Consider the following code snippet.
+	//	arrayOfInts[j] > arrayOfInts[j+1]
+//	Which operators does the code contain? ADDITIVE AND GREATER THAN
+
+//	Consider the following code snippet.
+	//	int i = 10;
+	//	int n = i++%5;
+		//	a. What are the values of i and n after the code is executed? 				10 AND 0	W/R: 11 AND 0
+		//	b. What are the final values of i and n if instead of using the postfix 
+			//	increment operator (i++), you use the prefix version (++i))? 			10 AND 1	W/R: 11 AND 1
+
+//	To invert the value of a boolean, which operator would you use? !
+//	Which operator is used to compare two values, = or == ?     ==
+//	Explain the following code sample: result = someCondition ? value1 : value2;
+	/*IF someCondition IS TRUE, IT WILL SHOW value1, OTHERWISE IT WILL SHOW value2*/
+	
+}
